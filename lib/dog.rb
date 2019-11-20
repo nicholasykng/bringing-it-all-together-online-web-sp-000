3a2f259bbd0ce0966dca3fc4f1e5da31a08586cd
Nicholas Ng <nicholasykng@gmail.com>
2019-11-20 04:30:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,9 +30,12 @@ class Dog
     end
 
     def self.find_by_name(name)
-      sql = "SELECT * FROM dogs WHERE name = ?"
-      result = DB[:conn].execute(sql, name)[0]
-      self.new(result[0], result[1], result[2])
+      sql = <<-SQL 
+      SELECT * FROM dogs WHERE name = ? LIMIT 1
+      SQL
+      DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+      end.first
     end
 
     def update
