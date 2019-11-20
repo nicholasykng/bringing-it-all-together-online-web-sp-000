1fcc39988d4f5bd09eb56cfbee2edbbe8ae24206
Nicholas Ng <nicholasykng@gmail.com>
2019-11-20 04:12:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,4 +29,10 @@ class Dog
       self.new(id: id, name: name, breed: breed)
     end
 
+    def self.find_by_name(name)
+      sql = "SELECT * FROM dogs WHERE name = ?"
+      result = DB[:conn].execute(sql, name)[0]
+      self.new(result[0], result[1], result[2])
+    end
+
 end
